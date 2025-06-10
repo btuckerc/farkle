0a8663806258a394e0e35f72c2a40df7e3323e96
btuckerc <btuckerc@users.noreply.github.com>
2025-06-10 09:19:01 -0400

    Initial Commit

---
+++ FarkleScorer/FarkleScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarkleScorerApp.swift
+//  FarkleScorer
+//
+//  Created by Tucker Craig on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FarkleScorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FarkleScorerTests/FarkleScorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FarkleScorerTests.swift
+//  FarkleScorerTests
+//
+//  Created by Tucker Craig on 6/10/25.
+//
+
+import Testing
+@testable import FarkleScorer
+
+struct FarkleScorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
